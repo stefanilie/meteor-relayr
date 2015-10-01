28288ce4b0de32c6b42e681b420168040eee7011
= <ilie.stefan92@yahoo.com>
2015-10-01 18:15:25 +0300

    working on plot

--- client/client.js
+++ client/client.js
@@ -16,6 +16,7 @@ Meteor.subscribe('humidPublications');
 
 GroupID = ""
 Created = false;
+var data = {};
 
 Template.body.helpers({
   luminosities: function() {
@@ -78,7 +79,7 @@ Template.grabData.events({
     // var readingType = document.getElementById("selectReadingType").value
     var clientID = "1e76f397-c940-4f88-a439-cfb4c950329d"
     var applicationID = "a683462a-9b29-453d-ab58-ef96293c70e4"
-    // var deviceID = "85fa69d0-8bb5-4400-94ea-ede7f960ca64"
+      // var deviceID = "85fa69d0-8bb5-4400-94ea-ede7f960ca64"
     var token = "1.t.wrb-k1N3hc5AR8uClh0elTZO-5HV"
     var htu = "85fa69d0-8bb5-4400-94ea-ede7f960ca64";
     var snd = "038d8561-5e1e-43ac-b504-48df027a1d9e";
@@ -152,7 +153,7 @@ Template.grabData.events({
       Meteor.call('getGroup', token, clientID, GroupID, function(err, results) {
         if (!err) {
           console.log(results);
-          alert("Group information:\nName:"+results['name']+"\nDevice count: "+results['devices'].length);
+          alert("Group information:\nName:" + results['name'] + "\nDevice count: " + results['devices'].length);
         }
       });
     }
@@ -177,12 +178,32 @@ Template.grabData.events({
   'click #history': function(event) {
     var clientID = "1e76f397-c940-4f88-a439-cfb4c950329d";
     var token = "1.t.wrb-k1N3hc5AR8uClh0elTZO-5HV";
+    var temps = [];
+    var times = [];
     Meteor.call('history', token, clientID, GroupID, function(err, results) {
       if (!err) {
+        var count = 1;
         var hist = JSON.parse(results['content']);
         for (var i = 0; i < hist.length; i++) {
-          console.log("Meaning: "+hist[i]['meaning']+"\nID: "+hist[i]['deviceId']);
+          if (hist[i]["meaning"] == "temperature") {
+            console.log("Meaning: " + hist[i]['meaning'] + "\nID: " + hist[i]['deviceId']);
+            console.log(hist[i]);
+            for (var j = 0; j < hist[i]['points'].length; j++) {
+              temps.push(Number(hist[i]['points'][j]['value']).toFixed(2));
+              times.push(hist[i]['points'][j]['timestamp']);
+            }
+            data[count] = {
+              temps: temps,
+              times: times,
+              deviceID: 'WB' + count
+            };
+            temps = [];
+            times = [];
+            count++;
+          }
         }
+        console.log(data);
+        ciorba(data);
       }
     });
   },
@@ -195,7 +216,7 @@ Template.grabData.events({
           console.log(results);
           alert("Deleted all groups!")
           document.getElementById('group').innerHTML = "1. Create group";
-          Created=false;
+          Created = false;
         }
       });
     } catch (e) {
@@ -217,3 +238,88 @@ Template.grabData.events({
     // });
   }
 });
+
+Template.areaDemo.onRendered = function() {
+  ciorba(data);
+};
+
+function ciorba(data){
+  $('#container-area').highcharts({
+        chart: {
+           type: 'area'
+       },
+
+       title: {
+           text: 'US and USSR nuclear stockpiles'
+       },
+
+       credits: {
+           enabled: false
+       },
+
+       subtitle: {
+           text: 'Source: <a href="http://thebulletin.metapress.com/content/c4120650912x74k7/fulltext.pdf">' +
+               'thebulletin.metapress.com</a>'
+       },
+
+       xAxis: {
+           allowDecimals: false,
+           labels: {
+               formatter: function () {
+                   return this.value; // clean, unformatted number for year
+               }
+           }
+       },
+
+       yAxis: {
+           title: {
+               text: 'Nuclear weapon states'
+           },
+           labels: {
+               formatter: function () {
+                   return this.value / 1000 + 'k';
+               }
+           }
+       },
+
+       tooltip: {
+           pointFormat: '{series.name} produced <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+       },
+
+       plotOptions: {
+           area: {
+               pointStart: 0,
+               marker: {
+                   enabled: false,
+                   symbol: 'circle',
+                   radius: 2,
+                   states: {
+                       hover: {
+                           enabled: true
+                       }
+                   }
+               }
+           }
+       },
+
+       series: [{
+           name: data[1]['deviceID'],
+           data: [null, null, null, null, null, 6, 11, 32, 110, 235, 369, 640,
+               1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468, 20434, 24126,
+               27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342, 26662,
+               26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
+               24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586,
+               22380, 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950,
+               10871, 10824, 10577, 10527, 10475, 10421, 10358, 10295, 10104]
+       }, {
+           name: data[2]['deviceID'],
+           data: [null, null, null, null, null, null, null, null, null, null,
+               5, 25, 50, 120, 150, 200, 426, 660, 869, 1060, 1605, 2471, 3322,
+               4238, 5221, 6129, 7089, 8339, 9399, 10538, 11643, 13092, 14478,
+               15915, 17385, 19055, 21205, 23044, 25393, 27935, 30062, 32049,
+               33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000, 37000,
+               35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
+               21000, 20000, 19000, 18000, 18000, 17000, 16000]
+       }]
+     });
+}
